6a7ea8530ea9a84b0faffba4d5f14e9ce96993a6
Hathaway-Shan <hathaway.shan@gmail.com>
2022-09-26 13:12:50 -0700

    Added BrowserRouter to index.js home page is now displaying

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import BookList from './components/book/BookList';
+//import React Router functions from React aliasing BrowserRouter to Router
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 
 function App() {
   // TODO: Add routes to books & views
   return (
-    <main className="container">
-      <h1>Library Catalog</h1>
-      <BookList />
-    </main>
+    <Router>
+      <main className="container">
+        <h1>Library Catalog</h1>
+        <Switch>
+          <Route path="/">
+            <BookList />
+          </Route>
+        </Switch>
+      </main>
+    </Router>
   );
 }
 
