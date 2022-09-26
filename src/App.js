9b3ce9bba39133c343c54e926aa11504d2f090bc
Hathaway-Shan <hathaway.shan@gmail.com>
2022-09-26 13:42:35 -0700

    added env and Home files

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import BookList from './components/book/BookList';
 //import React Router functions from React aliasing BrowserRouter to Router
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home/Home';
 
 function App() {
   // TODO: Add routes to books & views
@@ -10,8 +11,9 @@ function App() {
       <main className="container">
         <h1>Library Catalog</h1>
         <Switch>
-          <Route path="/">
-            <BookList />
+          <Route path="/" component={Home}>
+            <Home />
+            {/* <BookList /> */}
           </Route>
         </Switch>
       </main>
